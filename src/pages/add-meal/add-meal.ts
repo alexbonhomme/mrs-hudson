a17b945bfbc84d656d2b45620473a05021443631
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2017-05-08 16:41:43 +0200

    Implements data saving throug firebase

--- src/pages/add-meal/add-meal.ts
+++ src/pages/add-meal/add-meal.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
+import { NavController, LoadingController, ToastController } from 'ionic-angular';
 
+import { AppSettings } from '../../app/app.settings';
 import { FoodService } from '../../services/food.service';
+
+import { AngularFireDatabase } from 'angularfire2/database';
+import * as moment from 'moment';
 
 const AUTOCOMPLETE_MAX_ITEMS = 10;
 
@@ -16,18 +21,27 @@ export class AddMealPage {
   public foodItems: any[];
 
   public meal = {
-    data: '',
-    time: '',
+    datetime: moment().format('YYYY-MM-DDTHH:mmZ'),
     food: [],
     comment: ''
   };
 
   constructor(
-    private foodService: FoodService
+    private navCtrl: NavController,
+    private foodService: FoodService,
+    private db: AngularFireDatabase,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController
   ) {
+    // get food set
     this.foodService.getAll().subscribe(data => this.foodItemsOrigin = data);
   }
 
+  /**
+   * Autocomplete method. Filter `foodItems` list using given `value`.
+   *
+   * @param {string} value
+   */
   getItems(value: string) {
     // if the value is an empty string don't filter the items
     if (value && value.trim() !== '') {
@@ -41,7 +55,12 @@ export class AddMealPage {
     }
   }
 
-  addItem(item) {
+  /**
+   * Add a food item to `meal.food` array.
+   *
+   * @param {any[]} item
+   */
+  addItem(item: any[]) {
     this.meal.food.push(item);
 
     // reset query and clear autocomplete
@@ -49,11 +68,43 @@ export class AddMealPage {
     this.showAutocomplete = false;
   }
 
-  removeItemAt(index) {
+  /**
+   * Remove item from `meal.food` array.
+   *
+   * @param {number} index
+   */
+  removeItemAt(index: number) {
     this.meal.food.splice(index, 1);
   }
 
+  /**
+   * Save model to the database
+   */
   save() {
-    // TODO
+    let loader = this.loadingCtrl.create({
+      content: 'Enregistrement en cours...'
+    });
+
+    loader.present();
+
+    // bind object to database
+    this.db.
+      list(AppSettings.db.mealsList)
+      .push(this.meal)
+      .then(() => {
+        loader.dismiss();
+
+        // goto to listing page
+        this.navCtrl.pop();
+      })
+      .catch(error => {
+        loader.dismiss();
+        console.error(error);
+
+        this.toastCtrl.create({
+          message: "Oups, une erreur est survenue durant l'enregistrement des données ! :-(",
+          duration: 3000
+        }).present();
+      });
   }
 }
--- src/pages/meal/meal.ts
+++ src/pages/meal/meal.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AppSettings } from '../../app/app.settings';
 import { AddMealPage } from '../add-meal/add-meal';
+
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-meal',
   templateUrl: 'meal.html'
 })
 export class MealPage {
+  public meals: FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(
+    private navCtrl: NavController,
+    private db: AngularFireDatabase
+  ) {
+    this.meals = db.list(AppSettings.db.mealsList);
+  }
 
   addMeal() {
       this.navCtrl.push(AddMealPage);
